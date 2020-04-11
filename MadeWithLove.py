8f6789dd21d237e5e6bd99809ae642c84aab3903
khthu <khthu.tran16@gmail.com>
2020-04-11 11:55:28 +0700

    Change to.py

--- MadeWithLove.py
+++ MadeWithLove.py
@@ -1,6 +1,6 @@
 import csv
 
-with open(input("Enter file name")) as csvfile:
+with open(input("Enter file name: ")) as csvfile:
     readCSV = csv.reader(csvfile, delimiter = ',')
     for row in readCSV:
          print(row)
