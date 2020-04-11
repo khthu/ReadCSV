5d4890302bdce79a06459633f9c7f631ddbbe234
khthu <khthu.tran16@gmail.com>
2020-04-11 16:25:48 +0700

    Add export function

--- MadeWithLove.py
+++ MadeWithLove.py
@@ -1,6 +1,11 @@
 import csv
 
+#Open csv file
 with open(input("Enter file name: ")) as csvfile:
     readCSV = csv.reader(csvfile, delimiter = ',')
-    for row in readCSV:
-         print(row)
+    for row in readCSV: #Traverse the csv file
+         #Open txt file, or create one if not exist
+         with open('text.txt', 'a') as text_file:
+             writeTXT = csv.writer(text_file, delimiter = ',')
+             writeTXT.writerow(row) #Write each row to the new file
+text_file.close()
